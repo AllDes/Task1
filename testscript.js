7216ccbf15fb9a9f2254fdfff0d4e040d748b8bb
AllDes <koliadada@gmail.com>
2019-07-21 02:43:57 +0300

    FirstTask_2

--- testscript.js
+++ testscript.js
@@ -2,25 +2,19 @@
 
 
 let money = prompt("Your monthly budget?", "10"),
-    time = prompt("enter current date in format YYYY-MM-DD", "2019-07-21");
+    time = prompt("enter current date in format YYYY-MM-DD", "2019-07-21"),
+    c = prompt("Enter a mandatory expense item for this month.", ""),
+    d = prompt("How much will it cost?", "0");
 
 
 var appData = {
     budget: money,
     timeData: time,
-    expenses: {a,b},
+    expenses: {c,d},
     optionalExpenses: {},
     arrIncome: {},
     savings: false
     };
-
-let c = prompt("Enter a mandatory expense item for this month.", ""),
-    d = prompt("How much will it cost?", "0");
-
-appData.expenses.a = c;
-appData.expenses.b = d;
-
-
 
 alert(appData.budget/30);
 
